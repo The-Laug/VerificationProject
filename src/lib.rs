378f2feeb0131a40c6e2a89d62afe40831924e15
The-Laug <54498065+The-Laug@users.noreply.github.com>
2024-10-24 11:29:17 +0200

    temp push

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ use slang::ast::{Cmd, CmdKind, Expr, ExprKind, Ident, Name, Quantifier, Type, Va
 use slang::Span;
 use slang_ui::prelude::*;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
@@ -89,27 +91,27 @@ fn cmd_to_ivlcmd(cmd: &Cmd) -> Result<IVLCmd> {
         )),
         CmdKind::Assignment { name, expr } => Ok(IVLCmd::assign(name, expr)),
 
-        CmdKind::VarDefinition { name, ty: (_span, ty), expr }=>{ 
-            Ok(
-                match expr {
-                    Some(expr_value) => {
-                        // ask the ta
-                        //not sure if we should do the havoc before assign or assign is enough 
-                        //if we should perform the havoc before then IVLCmd::seq(&hav, &cmdd) should be called
-                        //but then the wp of assign is implemented twice so we should fix the implementation
-                        //the logic is true implementation is false
+        CmdKind::VarDefinition {
+            name,
+            ty: (_span, ty),
+            expr,
+        } => {
+            Ok(match expr {
+                Some(expr_value) => {
+                    // ask the ta
+                    //not sure if we should do the havoc before assign or assign is enough
+                    //if we should perform the havoc before then IVLCmd::seq(&hav, &cmdd) should be called
+                    //but then the wp of assign is implemented twice so we should fix the implementation
+                    //the logic is true implementation is false
 
-                        // let hav=IVLCmd::havoc(name, ty);                             
-                        let cmdd = IVLCmd::assign(name, expr_value);
-                        cmdd 
-                        // IVLCmd::seq(&hav, &cmdd)  
-                    },
-                    None => {
-                        IVLCmd::havoc(name, ty)
-                    }
+                    // let hav=IVLCmd::havoc(name, ty);
+                    let cmdd = IVLCmd::assign(name, expr_value);
+                    cmdd
+                    // IVLCmd::seq(&hav, &cmdd)
                 }
-            )
-    },
+                None => IVLCmd::havoc(name, ty),
+            })
+        }
 
         _ => todo!("Not supported (yet)."),
     }
@@ -128,15 +130,21 @@ fn init_map() -> HashMap<Ident, i32> {
     HashMap::new()
 }
 
-
-
-fn synchronize_cmd(com1:IVLCmd,map1:HashMap<Ident, i32>, map2: HashMap<Ident, i32> ) -> IVLCmd {
-    for (key,value1) in map1 {
+fn synchronize_cmd(com1: IVLCmd, map1: HashMap<Ident, i32>, map2: HashMap<Ident, i32>) -> IVLCmd {
+    for (key, value1) in map1 {
         if let Some(&value2) = map2.get(&key) {
             if value2 > value1 {
                 let new_ident = Ident(format!("{}{}", key, value2));
                 let old_ident = Ident(format!("{}{}", key, value1));
-                let assign = IVLCmd::assign(&Name::ident(new_ident), &Expr::ident(&old_ident, &Type::Unknown { name: Name::ident(old_ident.clone()) }));
+                let assign = IVLCmd::assign(
+                    &Name::ident(new_ident),
+                    &Expr::ident(
+                        &old_ident,
+                        &Type::Unknown {
+                            name: Name::ident(old_ident.clone()),
+                        },
+                    ),
+                );
                 IVLCmd::seq(&com1, &assign);
             }
         }
@@ -144,9 +152,12 @@ fn synchronize_cmd(com1:IVLCmd,map1:HashMap<Ident, i32>, map2: HashMap<Ident, i3
     com1
 }
 
-
 // Maybe not sure
-fn update_variable_map(variable_map: &mut HashMap<Ident, i32>, map1: &HashMap<Ident, i32>, map2: &HashMap<Ident, i32>) {
+fn update_variable_map(
+    variable_map: &mut HashMap<Ident, i32>,
+    map1: &HashMap<Ident, i32>,
+    map2: &HashMap<Ident, i32>,
+) {
     // Iterate over map1 and update variable_map
     for (key, &value1) in map1.iter() {
         let entry = variable_map.entry(key.clone()).or_insert(value1);
@@ -164,7 +175,6 @@ fn update_variable_map(variable_map: &mut HashMap<Ident, i32>, map1: &HashMap<Id
     }
 }
 
-
 // Updates a variable to the newest version according to the map
 fn update_variable_name(variable: &Ident, map: &mut HashMap<Ident, i32>) -> Ident {
     // Check if the variable exists in the map
@@ -179,7 +189,6 @@ fn update_variable_name(variable: &Ident, map: &mut HashMap<Ident, i32>) -> Iden
     Ident(new_variable_name)
 }
 
-
 // Code to make IVL commands to DSA form (Dynamic Single Assignment)
 // This code works by creating a map variable_map, which keeps track of all the variables and maps them to the number of times they occur in the program.
 // Using the variable_map, we can change the name of each of the variables, to the variablename concatenated with the number.
@@ -192,73 +201,83 @@ fn ivl_to_dsa(ivl: &IVLCmd, variable_map: &mut HashMap<Ident, i32>) -> Result<IV
         IVLCmdKind::Assignment { name, expr } => {
             let expr = (variable_map.iter().fold(expr.clone(), |acc, (var, &val)| {
                 let new_ident = Ident(format!("{}{}", var, val));
-                let new_expr = Expr::ident(&new_ident, &acc.ty);
+                let new_expr = Expr::ident(&new_ident, &Type::Int);
+                // THIS IS HARDCODED NEEDS TO BE CHANGED SPEAK TO TA ABOUT IT ^^^
                 acc.subst_ident(var, &new_expr)
             }));
             Ok(IVLCmd::assign(
                 &Name::ident(update_variable_name(&name.ident, variable_map)),
                 &expr,
             ))
-        },
+        }
+        // For assert we do the same as for assign except we only have an expression, not a new variable.
+        // Iterate through the variable_map
+        // For each variable look for and change the variable for the appropriate value
+        // Continue with the rest of the map
+        // NB. We use fold, because we want to use the output of the substitution to be the input of the next call of the fold function
+        IVLCmdKind::Assert { condition, message } => {
+            let new_condition = variable_map
+                .iter()
+                .fold(condition.clone(), |acc, (var, &val)| {
+                    let new_ident = Ident(format!("{}{}", var, val));
+                    let new_expr = Expr::ident(&new_ident, &Type::Int);
+                    // THIS IS HARDCODED NEEDS TO BE CHANGED SPEAK TO TA ABOUT IT ^^^
+                    acc.subst_ident(var, &new_expr)
+                });
+            Ok(IVLCmd::assert(&new_condition, &message.clone()))
+        }
+        // For assume we do the same as for assign except we only have an expression, not a new variable.
+        // Iterate through the variable_map
+        // For each variable look for and change the variable for the appropriate value
+        // Continue with the rest of the map
+        // NB. We use fold, because we want to use the output of the substitution to be the input of the next call of the fold function
+        IVLCmdKind::Assume { condition } => Ok(IVLCmd::assume(
+            &(variable_map
+                .iter()
+                .fold(condition.clone(), |acc, (var, &val)| {
+                    let new_ident = Ident(format!("{}{}", var, val));
+                    let new_expr = Expr::ident(&new_ident, &acc.ty);
+                    acc.subst_ident(var, &new_expr)
+                })),
+        )),
         // For the sequence we simply run the ivl_to_dsa for each of the commands
         // We assume that the rust program runs in sequential order, such that the variable_map gets updated by the first block before being used for the second one.
         IVLCmdKind::Seq(command1, command2) => Ok(IVLCmd::seq(
             &(ivl_to_dsa(command1, variable_map)?),
             &(ivl_to_dsa(command2, variable_map)?),
         )),
-        // For assert we do the same as for assign except we only have an expression, not a new variable.
-        // Iterate through the variable_map
-        // For each variable look for and change the variable for the appropriate value
-        // Continue with the rest of the map
-        // NB. We use fold, because we want to use the output of the substitution to be the input of the next call of the fold function
-        IVLCmdKind::Assert { condition, message } => Ok(IVLCmd::assert(&(variable_map
-            .iter()
-            .fold(condition.clone(), |acc, (var, &val)| {
-                let new_ident = Ident(format!("{}{}", var, val));
-                let new_expr = Expr::ident(&new_ident, &acc.ty);
-                acc.subst_ident(var, &new_expr)
-            })), &message.clone())
-            
-        ),
-        // For assume we do the same as for assign except we only have an expression, not a new variable.
-        // Iterate through the variable_map
-        // For each variable look for and change the variable for the appropriate value
-        // Continue with the rest of the map
-        // NB. We use fold, because we want to use the output of the substitution to be the input of the next call of the fold function
-        IVLCmdKind::Assume { condition } => Ok(IVLCmd::assume(&(variable_map
-            .iter()
-            .fold(condition.clone(), |acc, (var, &val)| {
-                let new_ident = Ident(format!("{}{}", var, val));
-                let new_expr = Expr::ident(&new_ident, &acc.ty);
-                acc.subst_ident(var, &new_expr)
-            })))),
         // For nondeterministic blocks we want to first compute the DSA of each individual command block
         // Then have a way of combining the resultant variable_maps, to make sure that we take the highest value for each variable
         // Finally we want to add assignments in the end of the blocks, to synchronize the variables.
         IVLCmdKind::NonDet(command1, command2) => {
             let map1 = &mut variable_map.clone();
             let map2 = &mut variable_map.clone();
-            let com1 = ivl_to_dsa(command1,  map1)?;
-            let com2 = ivl_to_dsa(command2,  map2)?;
+            let com1 = ivl_to_dsa(command1, map1)?;
+            let com2 = ivl_to_dsa(command2, map2)?;
             let done_com1 = synchronize_cmd(com1, map1.clone(), map2.clone());
             let done_com2 = synchronize_cmd(com2, map2.clone(), map1.clone());
-            update_variable_map(variable_map, map1,  map2);
-            Ok(IVLCmd::nondet(
-                &(done_com1),
-                &(done_com2),
+            update_variable_map(variable_map, map1, map2);
+            Ok(IVLCmd::nondet(&(done_com1), &(done_com2)))
+        }
+        IVLCmdKind::Havoc { name, ty } => {
+            // println!("name sent to IVLCmd::havoc: {:?}", name);
+            // println!("type sent to IVLCmd::havoc: {:?}", &ty.clone());
+            // println!("newname sent to IVLCmd::havoc: {:?}", &Name::ident(update_variable_name(&name.ident, variable_map)));
+            Ok(IVLCmd::havoc(
+                &Name::ident(update_variable_name(&name.ident, variable_map)),
+                &ty.clone(),
             ))
-        },
-        
+        }
         _ => todo!("Not supported (yet)."),
     }
 }
 
-
-
 // Weakest precondition of (assert-only) IVL programs comprised of a single assertion
 fn wp(ivl: &IVLCmd, postcon: &Expr) -> Result<(Expr, String)> {
     match &ivl.kind {
-        IVLCmdKind::Assert { condition, message } => Ok((condition.clone().and(postcon), message.clone())),
+        IVLCmdKind::Assert { condition, message } => {
+            Ok((condition.clone().and(postcon), message.clone()))
+        }
         // Assume has not been documented in the report yet
         // Here the wp of assume with the condition, C, takes the postcondition, G, and returns the weakest precondition:
         // I.e. : wp[assume C](G) = C -> G
@@ -268,32 +287,31 @@ fn wp(ivl: &IVLCmd, postcon: &Expr) -> Result<(Expr, String)> {
         // Seq has not been documented in the report yet
         // Here the wp of assume with the commands: command1 and command2 and the postcondition G returns the weakest precondition:
         // I.e. : wp[command1;command2](G) = wp[command1]( wp[command2](G) )
-        IVLCmdKind::Seq(command1, command2) => {
-            Ok((wp(command1, &wp(command2, postcon)?.0)?.0, "SEQ".to_string()))
-        },
+        IVLCmdKind::Seq(command1, command2) => Ok((
+            wp(command1, &wp(command2, postcon)?.0)?.0,
+            "SEQ".to_string(),
+        )),
         //After the code is transformed to dsa
         //we compute wp by assuming the assigment, for example if we have x:=3 we assume x==3
         // (name==expr) ==> postcond
-        IVLCmdKind::Assignment { name, expr } =>  {
-        Ok(((Expr::ident(&name.ident, &expr.ty).op(slang::ast::Op::Eq, expr)).imp(postcon), "Assignment".to_string()))
-        },
+        IVLCmdKind::Assignment { name, expr } => Ok((
+            (Expr::ident(&name.ident, &expr.ty).op(slang::ast::Op::Eq, expr)).imp(postcon),
+            "Assignment".to_string(),
+        )),
         //wp of havoc
         //the logic is true but we should make sure that span.Default() is true
-        IVLCmdKind::Havoc { name, ty }=>{
-            Ok((
-                Expr::quantifier(
-                    Quantifier::Forall, 
-                    &[Var {
-                        span: Span::default(),       
-                        name: name.clone(),     
-                        ty: (Span::default(), ty.clone()), 
-                    }],
-                    postcon
-                ),
-                "HERE".to_string()
-            ))
-        }
-        ,
+        IVLCmdKind::Havoc { name, ty } => Ok((
+            Expr::quantifier(
+                Quantifier::Forall,
+                &[Var {
+                    span: Span::default(),
+                    name: name.clone(),
+                    ty: (Span::default(), ty.clone()),
+                }],
+                postcon,
+            ),
+            "HERE".to_string(),
+        )),
         _ => todo!("Not supported (yet)."),
     }
 }
